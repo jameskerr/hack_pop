2d87b4be2fb87a5a4bb45982a137f207a3bd2657
jamiesunderland <jamesrainersunderland@gmail.com>
2016-09-21 19:31:02 -0700

    added Sqlite to hack pop ios

---
+++ mobile/hack pop/Carthage/Checkouts/SQLite.swift/build/SQLite.build/Release-iphoneos/SQLite iOS.build/Objects-normal/arm64/SQLite-Bridging.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/jamie/git/hack_pop/mobile/hack\ pop/Carthage/Checkouts/SQLite.swift/SQLite/Core/SQLite-Bridging.m \
+  /Users/jamie/git/hack_pop/mobile/hack\ pop/Carthage/Checkouts/SQLite.swift/SQLite/Core/SQLite-Bridging.h \
+  /Users/jamie/git/hack_pop/mobile/hack\ pop/Carthage/Checkouts/SQLite.swift/build/SQLite.build/Release-iphoneos/SQLite\ iOS.build/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS9.3.sdk/usr/include/sqlite3.h \
+  /Users/jamie/git/hack_pop/mobile/hack\ pop/Carthage/Checkouts/SQLite.swift/SQLite/Core/fts3_tokenizer.h
---
+++ mobile/hack pop/Carthage/Checkouts/SQLite.swift/build/SQLite.build/Release-iphoneos/SQLite iOS.build/Objects-normal/armv7/SQLite_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jamie/git/hack_pop/mobile/hack\ pop/Carthage/Checkouts/SQLite.swift/build/SQLite.build/Release-iphoneos/SQLite\ iOS.build/DerivedSources/SQLite_vers.c
