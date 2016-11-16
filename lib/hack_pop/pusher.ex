9898aef4ff331f97ac4239e1e263078c394f9dd1
James Kerr <jkerr@reflektive.com>
2016-11-15 21:25:43 -0800

    Refactored pusher to use in test endpoint

--- lib/hack_pop/pusher.ex
+++ lib/hack_pop/pusher.ex
@@ -13,24 +13,35 @@ defmodule HackPop.Pusher do
     Enum.each(clients, fn client ->
       Enum.each(stories, fn story ->
         if !already_sent?(client, story) && story.points >= client.threshold do
-          push(story, client)
+          story
+          |> create_notification(client)
+          |> push
         end
       end)
     end)
   end
 
-  def push(story, client) do
-    notification = Repo.insert! %Notification{client_id: client.id, story_id: story.id}
-    message = APNS.Message.new
-      |> Map.put(:token, notification.client_id)
-      |> Map.put(:alert, story.title)
-      |> Map.put(:badge, 0)
-      |> Map.put(:extra, %{
-        url: story.url,
-        id:  notification.id,
-        comments_url: story.comments_url
-        })
+  def push(notification) do
+    message = construct_message(notification)
     :ok = APNS.push(:dev_pool, message)
+  end
+
+  defp create_notification(story, client) do
+    %Notification{story_id: story.id, client_id: client.id}
+    |> Repo.insert!
+    |> Repo.preload(:story)
+  end
+
+  defp construct_message(notification) do
+    APNS.Message.new
+    |> Map.put(:token, notification.client_id)
+    |> Map.put(:alert, notification.story.title)
+    |> Map.put(:badge, 0)
+    |> Map.put(:extra, %{
+      url: notification.story.url,
+      id:  notification.id,
+      comments_url: notification.story.comments_url
+      })
   end
 
   defp already_sent?(client, story) do
