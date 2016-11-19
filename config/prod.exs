2f6df0e9d3c38998f29882bb03aa7353ccedf17b
James Kerr <jkerr@reflektive.com>
2016-11-18 21:08:53 -0800

    Allow the APNS_POOL to be configured by ENV vars

--- config/prod.exs
+++ config/prod.exs
@@ -7,6 +7,6 @@ config :hack_pop, HackPop.Repo,
   password: System.get_env("DB_PASSWORD"),
   hostname: System.get_env("DB_HOST")
 
-config :hack_pop, apns_pool: :prod_pool
+config :hack_pop, apns_pool: System.get_env("APNS_POOL") |> String.to_atom
 
 config :bugsnag, release_stage: System.get_env("BUGSNAG_RELEASE_STAGE")
